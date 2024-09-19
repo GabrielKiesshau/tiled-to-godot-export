39a5affcfad0e183da78bfce82b29745649c4225
Gabriel Kiesshau <gabriel.kiesshau@gmail.com>
2024-09-19 11:56:19 -0300

    Added Room object

---
+++ models/room.mjs
@@ -0,0 +1,40 @@
+import { Area2D } from './area_2d.mjs';
+import { Node2D } from './node_2d.mjs';
+import { Resource } from './resource.mjs';
+import { Vector2 } from './vector2.mjs';
+
+/**
+ * Represents a Room.
+ * @class Room
+ * @extends Node2D
+ */
+export class Room extends Area2D {
+  /**
+   * @param {Object} [props]
+   * @param {Resource} [props.data]
+   * @param {Vector2} [props.spawnPosition]
+   */
+  constructor({
+    data = null,
+    spawnPosition = new Vector2({ x: 0, y: 0 }),
+  } = {}) {
+    super();
+
+    /** @type {Resource} */
+    this.data = data;
+    /** @type {Vector2} */
+    this.spawnPosition = spawnPosition;
+
+    this.scriptPath = "addons/CustomNodeController/Room.cs";
+
+    this.setName("Room");
+    this.setZIndex(0);
+    this.setOwner(0);
+  }
+
+  getProperties() {
+    var properties = super.getProperties();
+
+    return properties;
+  }
+}
