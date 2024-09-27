f50b950b58c740c36a337cc2131bfc07e3c95e15
Gabriel Kiesshau <gabriel.kiesshau@gmail.com>
2024-09-26 21:40:48 -0300

    Updated CustomDataLayer class

--- models/custom_data_layer.mjs
+++ models/custom_data_layer.mjs
@@ -1,11 +1,24 @@
+/**
+* Represents a custom data layer.
+* @class PhysicsLayer
+*/
 export class CustomDataLayer {
+  /**
+   * @param {Object} [props]
+   * @param {string} [props.name]
+   * @param {string} [props.type]
+   * @param {number} [props.id]
+   */
   constructor({
     name = "Custom Data Layer",
     type = 0,
+    id = 0,
   } = {}) {
     /** @type {string} */
     this.name = name;
     /** @type {string} */
     this.type = type;
+    /** @type {number} */
+    this.id = id;
   }
 }
